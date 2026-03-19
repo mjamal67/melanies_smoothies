cb0b20a28e3a21f8426f0d3880b9248c617d9ccb
mjamal67 <jamalm05@yahoo.com>
2026-03-19 13:38:20 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,5 +64,5 @@ if ingredient_list:
 # st.text(smoothiefroot_response)
 # st.text(smoothiefroot_response.json())
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon") 
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+# smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon") 
+# sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
