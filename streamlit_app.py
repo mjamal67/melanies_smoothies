eaf6359c7fe066d94299f2bb2ccbce1088e17142
mjamal67 <jamalm05@yahoo.com>
2026-03-31 09:10:35 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 # Write directly to the app
 
-st.title(f":cup_with_straw: Customizessss Your Smoothie :cup_with_straw: ")
+st.title(f":cup_with_straw: Customize Your Smoothie :cup_with_straw: ")
 st.write(
   """Choose the fruits you want in your custom Smoothie!
   """
